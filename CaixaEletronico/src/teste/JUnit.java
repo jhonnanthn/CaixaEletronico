30a88786f8d97f9f0346967d834017599261aa3f
jhonnanthn <jhoonasw@hotmail.com.br>
2019-06-11 08:32:47 -0300

    Criação de novos testes (JUnit)

--- CaixaEletronico/src/teste/JUnit.java
+++ CaixaEletronico/src/teste/JUnit.java
@@ -2,9 +2,9 @@ package teste;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
 
 import controller.CedulaController;
 import entity.Cedula;
@@ -14,30 +14,45 @@ public class JUnit {
 	
 	@Before
     public void criarCedula() {
-        cedula = new Cedula(100.1);
+		System.out.println("Iniciar uma instancia de um objeto com o valor à sacar zerado.");
+        cedula = new Cedula();
     }
 	
 	@Test
 	public void test1() {
+		System.out.println("Teste 1 - Deverá retornar success.");
+        cedula = new Cedula(100.1);
 		CedulaController cedulaController = new CedulaController(cedula);
 		assertEquals("Entregar 1 nota de R$100.00; 0 de R$50.00; 0 de R$20.00; 0 de R$10.00", cedulaController.caixaEletronico());
 	}
 
 	@Test
 	public void test2() {
+		System.out.println("Teste 2 - Deverá retornar fail.");
+        cedula = new Cedula(120.1);
 		CedulaController cedulaController = new CedulaController(cedula);
 		assertEquals("Entregar 2 nota de R$100.00; 0 de R$50.00; 0 de R$20.00; 0 de R$10.00", cedulaController.caixaEletronico());
 	}
 	
 	@Test
 	public void test3() {
+		System.out.println("Teste 3 - Deverá retornar success.");
+        cedula = new Cedula(300);
 		CedulaController cedulaController = new CedulaController(cedula);
 		assertEquals("Entregar 3 nota de R$100.00; 0 de R$50.00; 0 de R$20.00; 0 de R$10.00", cedulaController.caixaEletronico());
 	}
 	
-	@AfterAll
+	@Test
 	public void test4() {
+		System.out.println("Teste 4 - Deverá retornar fail.");
+        cedula = new Cedula(400);
 		CedulaController cedulaController = new CedulaController(cedula);
-		assertEquals("0", cedulaController.saldo());
+		assertEquals("Entregar 3 nota de R$100.00; 0 de R$50.00; 0 de R$20.00; 0 de R$10.00", cedulaController.caixaEletronico());
 	}
+	
+	@After
+    public void finalizarCedula() {
+		System.out.println("Finalizar o teste, instanciando um objeto com o valor à sacar zerado.");
+        cedula = new Cedula();
+    }
 }
