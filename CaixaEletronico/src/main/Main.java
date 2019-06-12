a0f5a3f5beaa25b7beaafd5ff3a2780a48b2bd0a
jhonnanthn <jhoonasw@hotmail.com.br>
2019-06-12 10:45:59 -0300

    Ajuste Main

--- CaixaEletronico/src/main/Main.java
+++ CaixaEletronico/src/main/Main.java
@@ -10,10 +10,10 @@ public class Main {
 		double vlrASacar = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor a sacar: "));
 		Cedula cedula = new Cedula(vlrASacar);
 		CedulaController cedulaController = new CedulaController(cedula);
-		String resultado = cedulaController.caixaEletronico();
+		cedula = cedulaController.caixaEletronico();
 		System.out.println("//--------------------------------------------------------------------//");
-		System.out.println(resultado);
+		System.out.println(cedula.getResultado());
 		System.out.println("//--------------------------------------------------------------------//");
-		JOptionPane.showMessageDialog(null, resultado);
+		JOptionPane.showMessageDialog(null, cedula.getResultado());
 	}
 }
